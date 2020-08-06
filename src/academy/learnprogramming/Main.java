5d57271b80035226d42222c4f0abf997bcab3b4f
mari_s <ms.mari.suzuki@gmail.com>
2020-08-06 11:38:40 +0900

    in progress 253. Filesystem fixed the txt file names

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,12 +2,17 @@ package academy.learnprogramming;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
 
     public static void main(String[] args) {
+        Path path = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
+        printFile(path);
+        Path filePath = FileSystems.getDefault().getPath("files","SubdirectoryFile.txt");
+        printFile(filePath);
 
     }
 
