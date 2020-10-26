cda2182d1e1eceab6ba2079f23d1bfe3d0021ebb
mari_s <ms.mari.suzuki@gmail.com>
2020-10-26 20:33:39 +0900

    completed 257. File Attributes printed out the file attributes (the size is different from the lecture. Mine is 152 and the lecture is 151.)

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
@@ -14,8 +15,20 @@ public class Main {
 //            Files.createFile(fileToCreate);
 //            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
 //            Files.createDirectory(dirToCreate);
-            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
-            Files.createDirectories(dirToCreate);
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2/Dir3/Dir4/Dir5/Dir6");
+//            Files.createDirectories(dirToCreate);
+
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1/file1.txt");
+            long size = Files.size(filePath);
+            System.out.println("Size = " + size);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
+
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attrs.size());
+            System.out.println("Last modified  = " + attrs.lastModifiedTime());
+            System.out.println("Created = " + attrs.creationTime());
+            System.out.println("Is directory = " + attrs.isDirectory());
+            System.out.println("Is regular file = " + attrs.isRegularFile());
 
 //            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
 //            Files.deleteIfExists(fileToDelete);
