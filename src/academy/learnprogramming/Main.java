fcd525fdfd34a211e96cfdbdd7a9770da7aff286
mari_s <ms.mari.suzuki@gmail.com>
2020-10-26 20:13:45 +0900

    started 257. File Attributes deleted the try block from the previous lesson

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -9,20 +9,10 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.deleteIfExists(fileToDelete);
+//            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+//            Files.deleteIfExists(fileToDelete);
 
-//            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path destination = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Files.move(fileToMove, destination);
 
-//            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-//            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
-//
-//            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-//            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-//            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
         } catch(IOException e) {
             System.out.println(e.getMessage());
         }
